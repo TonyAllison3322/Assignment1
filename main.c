182b8c03e97b6ae89f803ff01de9384d395d14fc
TonyAllison3322 <49259914+TonyAllison3322@users.noreply.github.com>
2019-04-21 17:51:11 +1000

    Update main.c
    encyption works start the decryption tonight

--- main.c
+++ main.c
@@ -1,65 +1,116 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-
-
-    //  Tasks to complete rotational cipher. 5 to the right meaning that A = E 
-    //  1. Make key
-    //  2. Make encyption
-    //  3. Make decyption 
+    //Function prototypes
+    
+    char EIB(int letter, int key); //encryption in bounds of 56 - 90
+    char EOB(int letter, int key); //encryption out of bounds of 56 - 90
+    char DIB(int letter, int key); //decryption in bounds of 56 - 90
+    char DOB(int letter, int key); //decryption out of  bounds of 56 - 90
+    
     
     int main()
+    
 {
+    //Functions
 
-    FILE * file_pointer;                        //initalise a file for use
-    file_pointer = fopen("input.txt", "r"); // opens the file
-    if (file_pointer == NULL) {
-        perror("Error:");
-        return -1;
-    }
+    //Variables
     
-//    printf("Enter Message : \n");       // Starting Input   
-//    printf("Enter Rotation Key : \n");  // Starting Input For Key
-    
-    char letterE; // LetterE intialised for encypted letter
     int key;     // intialises key for future use
     char letter; // stores 'letter' as a char variable
-    char buffer[1];
+    int userchoice; 
+
+        /*-----------------User interface here-------------------------*/
+        /*printf("Hello, Which cipher would you like to use?\n\n1) Rotational\n2) Substitution\n");
+        scanf("%d", &userchoice);
+        switch(userchoice)
+            {
+            case '1': 
+            printf("You chose Rotational Cipher\n")
+            printf("")
+            }*/
+    FILE * file_pointer;                    //initalise a file for use
+    file_pointer = fopen("input.txt", "r"); // opens the file
     
-    // loop until the char left == 0 
+    if (file_pointer == NULL) 
+        {
+        perror("Error:");
+        return -1;
+        }
+    //fix the space bar key and star degryption
     
-    /*git test */
-    while (fscanf(stream, "%s", buffer) == 1) // expect 1 successful conversion
-    {
+    /*Encyption 
+   
+        fscanf(file_pointer,  "%d", &key); // enters value of key
+        printf("Key is %d\n\n", key);
         
-        fscanf(file_pointer, "%d %c", &key, &letter); // get the values from the input text file
-        printf("Key is %d and letter is %c\n", key, letter); // print the intial key and letter from the file
-
-    }
-    if (feof(stream)) 
-    {
-    break;
-    }
-    
+    while (!feof(file_pointer)) // expect 1 successful conversion
+        {
+        fscanf(file_pointer,  "%c", &letter); // get the values from the input text file
        
-    if(letter > 90 )
-    {
+        // Spacebar Block
+        
+       
+            if(letter == 32) {// 32 = space
+            printf("%c", letter);
+           }
+
+            else if  (letter > 90) // encyption
+            {  
+            letter = letter - 32; // turns the letter ASCII number from a lowercase to uppercase
+            if(letter + key > 90) 
+                {        
+                printf("%c", EOB(letter, key)); 
+                } 
+                    else
+                    {        
+                    printf("%c", EIB(letter, key)); 
+                    }
+            } 
+
+                       
             
-        letter = letter - 32; // turns the letter ASCII number from a lowercase to uppercase
-        printf("%d\n", letter); // prints the new upper case letter
-        letterE = letter + key; // inputs the key into the uppercase letter and stores it in encypted letterE
-        printf("The ASCII Value is %d, and the letter is %c\n", letterE, letterE); //Prints the encypted letter as well as the encypted ASCII value
-    
-    }
-    else 
-    {
-       
-        printf("%d\n", letter); // prints the new upper case letter
-        letterE = letter + key; // inputs the key into the uppercase letter and stores it in encypted letterE
-        printf("The ASCII Value is %d, and the letter is %c\n", letterE, letterE); //Prints the encypted letter as well as the encypted ASCII value
-    
-    }   
+            //Uppercase Block  
+            else if (letter <91)
+            {
+            printf("%c", EIB(letter, key)); 
+        }
 
-
+                else 
+                {        
+                printf("%c", letter); //printf("%c", EOB(letter, key)); 
+                } 
+               
+        
+ } */
 fclose(file_pointer); //closes the file
 return(0);
-} 
+
+}
+//Function Definitions
+
+char EIB(int letter, int key)
+{
+    char letterE;
+    letterE = letter + key;
+    return(letterE);
+}
+
+char EOB(int letter, int key)
+{
+    char letterE;
+    letterE = letter + key - 26;
+    return(letterE);
+}
+
+char DIB(int letter, int key)
+{
+    char letterD;
+    letterD = letter - key;
+}
+
+char DOB(int letter, int key)
+{
+    char letterD;
+    letterD = letter - 32;
+}
