9b67f8eef1c23b6e784a6c2069b5477f7753f72a
TonyAllison3322 <49259914+TonyAllison3322@users.noreply.github.com>
2019-04-28 21:49:24 +1000

    Update main.c

--- main.c
+++ main.c
@@ -10,7 +10,8 @@
     char DOB(char letter, int key); //decryption out of  bounds of 56 - 90
     char DWK(char letter, int key); //rotation decryption with key
     char EWK(char letter, int key); //Rotation encryption with key
-    char SUBWK(char letter, int i); //Substitution encryption with key
+    char SUBEWK(char letter, int i); //Substitution encryption with key
+    char SUBDWK(char letter, int i); //Substitution decryption with key
     int main()
     
 {
@@ -20,9 +21,11 @@
     char letter; // stores 'letter' as a char variable
     int userchoice; 
     char subkey[26];
-    //char message[10][150],buffer[150];
     int i ;
     char ch;
+    char *p;
+    int index;
+    char letterN;
     
     FILE * subkeyinput;
         subkeyinput = fopen("subkeyinput.txt", "r");
@@ -52,17 +55,68 @@
          //fprintf(output, "Key is %d\n", key);
          //printf("%c", EWK(letter, key));
          //printf("%c", DWK(letter, key));
-         printf("%c", SUBWK(letter, i));
-         
-         
-        
- 
+         //printf("%c", SUBEWK(letter, i));
+         //printf("%c", SUBDWK(letter, i));
+    while(!feof(subinput))
+    {
+        fscanf(subkeyinput, "%s", subkey);
+        fscanf(subinput, "%c", &letter);
+        if(letter >= 'A' && letter <= 'Z')
+        {
+            printf("%c\t\t", letter);
+            
+            p = strchr(subkey, letter);
+            printf("%d\t", p);
+            index = (int)(p - subkey);
+            printf("%d\t\t",index);
+            fseek( subkeyinput, index , SEEK_SET);
+            letterN = index + 65;
+            printf("%c\n", letterN);
+            p = 0;
+        }
+        else if(letter >= 'a' && letter <= 'z')
+        {
+            letter = letter - 32;
+            char *p;
+            int index;
+            p = strchr(subkey, letter);
+            index = (int)(p - subkey);
+            fseek( subkeyinput, index , SEEK_SET);
+            letterN = index + 65;
+            printf("%c\n", letterN);
+            
+        }
+        else
+        {
+            letterN = letter;
+            printf("%c\n", letter);
+            p = 0;
+        } 
+    }
 
+
+    
+    
+   
+
+   
+  /* const char c;
+   char *p;
+        while (!feof(subinput))
+        {
+            fscanf(subkeyinput, "%s", subkey);  // reads key file as a string
+            fscanf(subinput, "%c", &letter); // scans the text file for a char
+           
+            p = strchr(subkey, letter);
+            
+            printf("%c", p);
+        }
+*/
 fclose(output);       //closes the file
 fclose(file_pointer); //closes the file
+
 return(0);
 }
-
 
 //Function Definitions
 
@@ -203,7 +257,7 @@ char EWK(char letter, int key)
         } 
         return(letter);
 }
-char SUBWK(char letter, int i) 
+char SUBEWK(char letter, int i) 
 {
     
     
@@ -243,6 +297,6 @@ char SUBWK(char letter, int i)
             {
                 printf("%c", letter);
             }
-
-        } 
+        }
 }
+
