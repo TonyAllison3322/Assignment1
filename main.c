30e3bf6bc065a0416896fd9e5b4d0bd70a077d78
TonyAllison3322 <49259914+TonyAllison3322@users.noreply.github.com>
2019-04-28 15:29:51 +1000

    Update main.c

--- main.c
+++ main.c
@@ -4,37 +4,30 @@
 
     //Function prototypes
     
-    char EIB(int letter, int key); //encryption in bounds of 56 - 90
-    char EOB(int letter, int key); //encryption out of bounds of 56 - 90
-    char DIB(int letter, int key); //decryption in bounds of 56 - 90
-    char DOB(int letter, int key); //decryption out of  bounds of 56 - 90
-    char DWK(int letter, int key); //decryption with key
-    
+    char EIB(char letter, int key); //encryption in bounds of 56 - 90
+    char EOB(char letter, int key); //encryption out of bounds of 56 - 90
+    char DIB(char letter, int key); //decryption in bounds of 56 - 90
+    char DOB(char letter, int key); //decryption out of  bounds of 56 - 90
+    char DWK(char letter, int key); //decryption with key
+    char EWK(char letter, int key); //encryption with key
     int main()
     
 {
-    //Functions
 
     //Variables
-    char message[100];
-    int key = 1;     // intialises key for future use
+    int key;     // intialises key for future use
     char letter; // stores 'letter' as a char variable
     int userchoice; 
-    int s = 1; //shift
-    int i; // increment
-    char letterN; //New message
-        /*-----------------User interface here-------------------------*/
-        /*printf("Hello, Which cipher would you like to use?\n\n1) Rotational\n2) Substitution\n");
-        scanf("%d", &userchoice);
-        switch(userchoice)
-            {
-            case '1': 
-            printf("You chose Rotational Cipher\n")
-            printf("")
-            }*/
-    FILE * file_pointer;                    //initalise a file for use
-    file_pointer = fopen("input.txt", "r"); // opens the file
-    
+
+    FILE * output;
+    output = fopen("rotkeyoutput.txt", "w"); 
+    if (output == NULL) 
+        {
+        perror("Error:");
+        return -1;
+        }  
+    FILE * file_pointer;                    // intialising and opening of key input text file
+    file_pointer = fopen("rotkeyinput.txt", "r");
     if (file_pointer == NULL) 
         {
         perror("Error:");
@@ -42,81 +35,14 @@
         }
         
         // Decryption with key 
-        // printf("%d : ", i);
-        while(!feof(file_pointer))
-        {
-            fscanf(file_pointer, "%s", message[i]);
-            printf("%c\t", message[i]);
-            printf("boo");
-         }  
-           /* if(letter >=65 && letter <=90)
-            {
-                printf("%c", DWK(letter, key));
-            }
-            
-            else
-            {
-                printf("%c", letter);
-            }
+         fscanf(file_pointer,  "%d", &key);
+         fprintf(output, "Key is %d\n", key);
+         //printf("%c", EWK(letter, key));
+         //printf("%c", DWK(letter, key));
+         
+         
 
-        }
-      
-printf("\n");
-            
-        
-*/
-
-      //      printf("\n");
-      // printf("finished");
-     
-    /*Encyption 
-   
-        fscanf(file_pointer,  "%d", &key); // enters value of key
-        printf("Key is %d\n\n", key);
-        
-    while (!feof(file_pointer)) // expect 1 successful conversion
-        {
-        fscanf(file_pointer,  "%c", &letter); // get the values from the input text file
-       
-        // Spacebar Block
-        
-       
-          
-
-            if  (letter >= 96 && letter <=122) // encyption
-            {  
-            letter = letter - 32; // turns the letter ASCII number from a lowercase to uppercase
-            if(letter + key > 90) 
-                {        
-                printf("%c", EOB(letter, key)); 
-                } 
-                    else
-                    {        
-                    printf("%c", EIB(letter, key)); 
-                    }
-            } 
-
-                       
-            
-            //Uppercase Block  
-            else if (letter >=65 && letter <=90)
-            {
-            printf("%c", EIB(letter, key)); 
-        
-
-                if(letter + key > 90) 
-                {        
-                printf("%c", letter); //printf("%c", EOB(letter, key)); 
-                } 
-               } 
-             else
-             {
-                 printf("%c", letter);
-             }
-    
-
- } 
-*/
+fclose(output);       //closes the file
 fclose(file_pointer); //closes the file
 return(0);
 }
@@ -124,35 +50,136 @@ return(0);
 
 //Function Definitions
 
-char EIB(int letter, int key)
+char EIB(char letter, int key)
 {
     char letterE;
     letterE = letter + key;
     return(letterE);
 }
 
-char EOB(int letter, int key)
+char EOB(char letter, int key)
 {
     char letterE;
     letterE = letter + key - 26;
     return(letterE);
 }
 
-char DIB(int letter, int key)
+char DIB(char letter, int key)
 {
     char letterD;
     letterD = letter - key;
 }
 
-char DOB(int letter, int key)
+char DOB(char letter, int key)
 {
     char letterD;
-    letterD = letter - 26;
+    letterD = letter - key + 26;
 }
 
-char DWK(int letter, int key)
+char DWK(char letter, int key)
 {
-    char letterN;
-    letterN = letter - key;
-    return letterN;
+FILE * outputtext;
+    outputtext = fopen("rotoutput.txt", "w");
+    FILE * input;                    // intialising and opening of input text file
+    input = fopen("rotinput.txt", "r"); 
+    FILE * output;
+    output = fopen("rotkeyoutput.txt", "w");
+    FILE * file_pointer;                    // intialising and opening of key input text file
+    file_pointer = fopen("rotkeyinput.txt", "r");
+
+   fscanf(file_pointer,  "%d", &key); // enters value of key
+        printf("Key is %d\n", key);
+        
+    while (!feof(input)) // expect 1 successful conversion
+        {
+         fscanf(input,  "%c", &letter); // get the values from the input text file
+            if(letter >=65 && letter <=90)
+                {
+                    
+                    if(letter + key < 'A' )
+                    {
+                        printf("%c", DOB(letter, key)); 
+                        fprintf(outputtext, "%c", EOB(letter, key));
+                    }
+                    else
+                    {
+                        printf("%c", DIB(letter, key));
+                        fprintf(outputtext, "%c", DIB(letter, key));
+                    }
+                }
+            
+            else if(letter >= 'a' && letter <= 'z')
+                {
+                    letter = letter - 32;
+                    if(letter - key < 'A' )
+                    {
+                        printf("%c", DOB(letter, key));
+                        fprintf(outputtext, "%c", DOB(letter, key));
+                    }
+                    else
+                    {
+                        printf("%c", DIB(letter, key));
+                        fprintf(outputtext, "%c", DIB(letter, key));
+                    }
+                }
+            else
+                {
+                    printf("%c", letter);
+                    fprintf(outputtext, "%c", letter);
+                }
+        } 
+}
+
+char EWK(char letter, int key)
+{
+    FILE * outputtext;
+    outputtext = fopen("rotoutput.txt", "w");
+    FILE * input;                    // intialising and opening of input text file
+    input = fopen("rotinput.txt", "r"); 
+    FILE * output;
+    output = fopen("rotkeyoutput.txt", "w");
+    FILE * file_pointer;                    // intialising and opening of key input text file
+    file_pointer = fopen("rotkeyinput.txt", "r");
+
+   fscanf(file_pointer,  "%d", &key); // enters value of key
+        printf("Key is %d\n", key);
+        
+    while (!feof(input)) // expect 1 successful conversion
+        {
+         fscanf(input,  "%c", &letter); // get the values from the input text file
+            if(letter >=65 && letter <=90)
+                {
+                    
+                    if(letter + key > 'Z' )
+                    {
+                        printf("%c", EOB(letter, key)); 
+                        fprintf(outputtext, "%c", EOB(letter, key));
+                    }
+                    else
+                    {
+                        printf("%c", EIB(letter, key));
+                        fprintf(outputtext, "%c", EIB(letter, key));
+                    }
+                }
+            
+            else if(letter >= 'a' && letter <= 'z')
+                {
+                    letter = letter - 32;
+                    if(letter - key > 'Z' )
+                    {
+                        printf("%c", EOB(letter, key));
+                        fprintf(outputtext, "%c", EOB(letter, key));
+                    }
+                    else
+                    {
+                        printf("%c", EIB(letter, key));
+                        fprintf(outputtext, "%c", EIB(letter, key));
+                    }
+                }
+            else
+                {
+                    printf("%c", letter);
+                    fprintf(outputtext, "%c", letter);
+                }
+        } 
 }
